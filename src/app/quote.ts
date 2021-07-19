bb6b71ffd63514fed3ca9adfa194f0fb1955342b
cyruswere <cyrus.were@student.moringaschool.com>
2021-07-19 11:21:31 +0300

    made changes

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,11 @@
 export class Quote {
-    public showDescription: boolean;
-    constructor(public id: number,public name: string,public description: string, public completeDate: Date,public like: number,public unlike: number){
+
+  public showDescription: boolean ;
+  like:number;
+  unlike:number;
+  constructor(public id: number,public name: string,public description: string, public completeDate:Date){
       this.showDescription=false;
-    }
+      this.like=0;
+      this.unlike=0;
+  }
 }
